035447112a3a2f945dbaa396099d524a74c902b2
abrarr21 <abrarrr2110@gmail.com>
2024-08-24 12:49:18 +0530

    categorization

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { productContext } from '../utils/Context'
+import { Link } from 'react-router-dom';
 
 function Navbar() {
+
+  const [products] = useContext(productContext)
+  
+  let unique_category = 
+        products && products.reduce((accumulator, currentValue) => [...accumulator, currentValue.category], []);
+
+  unique_category = [...new Set(unique_category)] 
+  
+  console.log(unique_category)
+
+  const randomColor = () => {
+    return `rgba(${(Math.random()*255)}, ${(Math.random()*255)}, ${(Math.random()*255)}, 0.4)`
+  }
+
+
   return (
     <nav className="w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5">
 
@@ -8,22 +25,17 @@ function Navbar() {
       <hr className="my-3 w-[80%]" />
 
       <h1 className="text-2xl mb-3 w-[80%]">Category Filter</h1>
-      <ul className=" w-[80%]">
-        <li className="flex items-center mb-3">
-          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-red-400"></span>{" "} 
-          cat 1 
-        </li>
 
-        <li className="flex items-center mb-3">
-          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-teal-400"></span>{" "} 
-          cat 1 
-        </li>
+      <div className=" w-[80%]">
+        {unique_category.map((category, index) => (
+          <Link 
+            key={index}
+            to={`/?category=${category}`} className="flex items-center mb-3">
+            <span style={{backgroundColor: randomColor( )}} className="rounded-full mr-2 w-[15px] h-[15px]"></span>{" "} 
+            {category} 
+        </Link>))}
 
-        <li className="flex items-center mb-3">
-          <span className="rounded-full mr-2 w-[15px] h-[15px] bg-purple-300"></span>{" "} 
-          cat 1 
-        </li>
-      </ul>
+      </div>
     </nav>
   )
 }
